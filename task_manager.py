719bb20eaaec58c254aa6c6c1b081e6e70bc0ee1
serg-yakovlev <sergey.ya@gmail.com>
2019-12-04 20:23:29 +0300

    psutil

--- task_manager.py
+++ task_manager.py
@@ -1,4 +1,5 @@
 import gi
+import psutil
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GdkPixbuf
 import os, time, re
@@ -42,35 +43,53 @@ class MainWindow(Gtk.Window):
                 #print(pid[0], command[0])
         return [pids, commands]
 
+    def proc_list(self):
+        processes = []
+        for proc in psutil.process_iter():
+            try:
+                e = proc.exe()
+            except psutil.AccessDenied:
+                e = None
+            proc_dict = proc.as_dict()
+            p = {
+                'id': proc.pid,
+                'name': proc_dict['name'],
+                'username': proc_dict['username'],
+                'file': e,
+                'memory': proc_dict['memory_percent'],
+                'mem_info': proc.memory_info()
+                }
+            processes.append(p)
+            #print(
+            #    type(p['id']),
+            #    type(p['name']),
+            #    type(p['username']),
+            #    type(p['file']),
+            #    type(p['memory']),
+            #    type(p['mem_info'])
+            #    )
+        processes.sort(key=lambda x: x['memory'], reverse=True)
+        return processes
+
     def create_tree(self):
-        pids, commands = self.create_process_list()
-        store = Gtk.ListStore(int, str)
-        for pid, comm in zip(pids, commands):
-            store.append([pid, comm])
+        processes = self.proc_list()
+        store = Gtk.ListStore(int, str, str, str, float)
+        for proc in processes:
+            store.append([
+                proc['id'],
+                proc['name'],
+                proc['username'],
+                proc['file'],
+                proc['memory']
+                ])
         model = store.filter_new()
         treeview = Gtk.TreeView.new_with_model(model)
-        column_names = ["pid", "command"]
+        column_names = ["pid", "name", "username", "file", "memory %"]
         for i, col_n in enumerate(column_names):
             renderer = Gtk.CellRendererText()
             column = Gtk.TreeViewColumn(col_n, Gtk.CellRendererText(), text=i)
             treeview.append_column(column)
         return treeview
-
-
-
-
-class Callback:
-    def __init__(self, func, *args, **kwargs):
-        self.func = func
-        self.args = args
-        self.kwargs = kwargs
-    def __call__(self, args):
-        self.func(*self.args, **self.kwargs)
-
-
-
-
-
 
 
 if __name__ == '__main__':
